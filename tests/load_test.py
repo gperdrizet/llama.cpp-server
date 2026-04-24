6beaf821a82116614c5dc9b1014777171f425f82
George Perdrizet <george@perdrizet.org>
2026-04-24 10:50:01 -0400

    Cleaned up

--- tests/load_test.py
+++ tests/load_test.py
@@ -96,7 +96,11 @@ async def chat_request(
 
             if resp.status != 200:
                 body = await resp.text()
-                return {'error': f'HTTP {resp.status}: {body}', 'latency': None, 'ttft': None, 'tokens': 0}
+
+                return {
+                    'error': f'HTTP {resp.status}: {body}',
+                    'latency': None, 'ttft': None, 'tokens': 0
+                }
 
             if stream:
 
